e3d759400809495eb6cc92c597480b0f73acb13d
rabby7701 <rabby771islam@gmail.com>
2026-03-11 10:44:07 +0600

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T04:43:25.527Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T04:44:20.424Z
+Commit: Resolve null pointer exception in user service
