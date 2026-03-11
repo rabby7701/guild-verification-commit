8b22c2b597005c77a8b17ff3e45471e8a6b96567
rabby7701 <rabby771islam@gmail.com>
2026-03-11 10:44:39 +0600

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T04:44:35.441Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-11T04:44:52.729Z
+Commit: Fix memory leak in event handlers
